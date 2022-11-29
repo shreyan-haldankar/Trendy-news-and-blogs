1a8f4d61f4f6f294810f306b07ae2981b55a4ad9
Shreyan <shreyanhaldankar@gmail.com>
2022-11-29 11:14:09 +0530

    Added a Rich Text Editor for Blog Posts

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from news.models import Profile
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    content = models.TextField()
+    # content = models.TextField()
+    content = RichTextField(blank=True, null=True)
     author = models.ForeignKey(Profile,  on_delete=models.CASCADE, related_name="blog_posts")
     published = models.DateTimeField(default=timezone.now)
     
--- news_blog_app/settings.py
+++ news_blog_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'rest_framework',
     "corsheaders",
+    'ckeditor',
 
 ]
 
